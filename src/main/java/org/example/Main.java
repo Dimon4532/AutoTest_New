979b5ed94c2cb4d29be11a1f47a7a9c57ca7d2db
Dimon4532 <pop7k64@gmail.com>
2025-05-25 16:46:02 +0300

    Add Getters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,45 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+
+
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-        cycle();
+
+        cycle( 10);
     }
 
-    private static void cycle() {
-        for (int i = 1; i <= 4; i++) {
+    // метод cycle_for
+    private static List<Integer> cycle(int count) {
+        List<Integer> listNumbers = new ArrayList<>(count);
+
+        for (int i = 0; i < count; i++) {
+
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+  //          System.out.println("i = " + i);
+            listNumbers.add(i);
+ //           if (i % == 0) {
+ //               System.out.println("even");
+ //           }   else if (i % 2 ==1) {
+ //               System.out.println("not-even");
+ //           }   else {
+ //               System.out.println("odd");
+ //           }
+ //           String result = (i % 2 == 0) ? "event" : "not-event";
+ //           System.out.println(result);
         }
+        System.out.println("listNumber = " + listNumbers);
+
+        return listNumbers;
     }
 }
--- src/main/java/org/example/Sender.java
+++ src/main/java/org/example/Sender.java
@@ -1,12 +1,31 @@
 package org.example;
 
 public class Sender {
-    private String url;
-    private String path;
-    private String body;
+    private final String url;
+    private final String path;
+    private final String body;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getBody() {
+        return body;
+    }
 
     // Конструктор с параметрами
     public Sender(String url, String path, String body) {
+        if (url == null || url.isEmpty()) {
+            throw new IllegalArgumentException("url is null or empty");
+        }
+        if (path == null || path.isEmpty()) {
+            throw new IllegalArgumentException("path is null or empty");
+        }
+
         this.url = url;
         this.path = path;
         this.body = body;
@@ -26,6 +45,14 @@ public class Sender {
                 actualBody);
     }
 
+    public String send(String url, String path) {
+        return send(url,path,  "test body");
+    }
+
+    public String send() {
+        return send( null,  null,  null);
+    }
+
 
     // Пример использования
     public static void main(String[] args) {
@@ -33,6 +60,8 @@ public class Sender {
 
         // Используем параметры метода
         System.out.println(sender.send("https://github.com", "/Dimon4532/AutoTest_New", "body"));
+        System.out.println(sender.send("https://github.com", "/Dimon4532/AutoTest_New"));
+        System.out.println(sender.send());
         // Вывод: Отправлено на google.com/search с телом: query
 
     }
